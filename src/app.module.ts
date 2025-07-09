9c16df1e5c217fde3963a5f38e1d0161da23688f
Karolis Jakas <karolis@IT-PC07.PGO.UCAM>
2025-07-09 17:32:10 +0200

    added health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, HealthModule],
 })
 export class AppModule {}
